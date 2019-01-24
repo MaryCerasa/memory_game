0807f601fbbed3ec1056e217ca1b22e140b9e59c
Mary Cerasa <marycerasa@Marys-MacBook-Pro.local>
2019-01-24 09:31:36 +0000

    created game logic

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardFour);
+var cards;
+var cardsInPlay;
+var cardOne;
+var cardTwo;
+var cardThree;
+var cardFour;
+
+var cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+cardOne = cards[0];
+cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+	console.log(checkforMatch);
+} 
+else if (cardsInPlay[0] === cardsInPlay [1]) {
+	alert("You found a match!");
+}
+else (cardsInPlay [0] != cardsInPlay[1]) {
+	alert("Sorry, try again.");
+}
