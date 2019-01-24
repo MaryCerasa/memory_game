215c05de34fb316bb98a88a53885f419240613e7
Mary Cerasa <marycerasa@Marys-MacBook-Pro.local>
2019-01-24 11:33:24 +0000

    created game logic

--- main.js
+++ main.js
@@ -5,7 +5,33 @@ var cardTwo;
 var cardThree;
 var cardFour;
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+
+{
+	rank: "king",
+	suit: "hearts"
+	cardImage: "images/king-of-hearts.png",
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+
+];
+
 var cardsInPlay = [];
 
 var checkForMatch= function() {
@@ -15,16 +41,20 @@ if (cardsInPlay[0] === cardsInPlay [1]) {
 }
 else {
 	console.log("Sorry, try again.");
-}
+};
+
+};
 
 var flipCard= function(cardId) {
-console.log("User flipped" + cards[cardId])
+console.log("User flipped" + cards[cardId].rank)
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cardImage)
 
 if (cardsInPlay.length === 2) {
 	console.log(checkForMatch);
-} 
+};
 
 checkForMatch();
 
